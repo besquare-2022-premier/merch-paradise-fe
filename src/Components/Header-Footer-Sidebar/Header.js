0e5ddc28aee245034cf4617d162337b052c96929
Ikaa <nurul.syafiqah@besquare.com.my>
2022-11-10 23:52:19 +0800

    class change to className

--- src/Components/Header-Footer-Sidebar/Header.js
+++ src/Components/Header-Footer-Sidebar/Header.js
@@ -4,47 +4,57 @@ import "../Header-Footer-Sidebar/Header.css";
 
 function Header() {
   return (
-    <div class="container">
-      <nav class="w-background">
-        <div class="nav-left">
-          <a>
-            <img src="./img/MP_logo.svg"></img>
-          </a>
-          <Link to="/">
-            <h5>Merch Paradise</h5>
-          </Link>
-        </div>
-        <div class="menu-li">
-          <ul>
-            <li>
-              <Link to="/categories">Categories</Link>
-            </li>
-            <li>
-              <Link to="/community">Community</Link>
-            </li>
-            <li>
-              <Link to="/support">Support</Link>
-            </li>
-          </ul>
-        </div>
-        <div class="nav-right">
-          <ul>
-            <li>
-              <input type="text" placeholder="Search here"></input>
-            </li>
-            <li>
-              <button class="button-secondary">
-                <Link to="/login">Log in</Link>
-              </button>
-            </li>
-            <li>
-              <button class="button-primary">
-                <Link to="/register">Register</Link>
-              </button>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    <div className="container">
+      <div className="nav-container w-background">
+        <nav className="nav">
+          <div className="nav-left">
+            <div className="header-logo">
+              <div className="d-flex h-center">
+                <img src="./img/MP_logo.svg"></img>
+                <Link to="/shop">
+                  <a>Merch Paradise</a>
+                </Link>
+              </div>
+            </div>
+            <div className="menu-li">
+              <ul className="menu">
+                <li>
+                  <a>
+                    <Link to="/shop/categories">Categories</Link>
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    <Link to="/shop/community">Community</Link>
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    <Link to="/shop/support">Support</Link>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="nav-right">
+            <ul>
+              <li>
+                <input type="text" placeholder="Search here"></input>
+              </li>
+              <li>
+                <button className="button-secondary">
+                  <Link to="/login">Log in</Link>
+                </button>
+              </li>
+              <li>
+                <button className="button-primary">
+                  <Link to="/register">Register</Link>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
