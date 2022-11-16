ad00333c35a31b278e445b3df550ff886bc90f0d
Jia Jun Ngeo <jia.jun@besquare.com.my>
2022-11-16 18:54:42 +0900

    pages: finished up the signup email page

--- src/Components/emailSignup/SignupContainer/SignupContainer.js
+++ src/Components/emailSignup/SignupContainer/SignupContainer.js
@@ -1,16 +1,51 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import './SignupContainer.css'
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { ENDPOINT_BASE } from "../../../store/__base/config";
+import { obtainCSRF } from "../../../store/__base/csrf";
+import { generateCSRFHeader } from "../../../store/__base/headerUtils";
+import { fetchJsonWithCookie } from "../../../utils/fetch";
+import { JumpingRabbitLoader } from "../../common/Loader";
+import { ValidatingInputField } from "../../common/ValidatingInputField";
+import "./SignupContainer.css";
 
 export default function SignupContainer() {
-      const [email, setEmail] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
 
-      const [shouldRedirect, setShouldRedirect] = React.useState(false);
+  const [shouldRedirect, setShouldRedirect] = React.useState(false);
 
-      if (shouldRedirect) {
-        return <Navigate to="/login" />;
-      }
-
+  if (shouldRedirect) {
+    return <Navigate to="/login" />;
+  }
+  /**
+   *
+   * @param {React.FormEvent<HTMLFormElement>} e
+   */
+  function performSignup(e) {
+    e.preventDefault();
+    if (!e.nativeEvent.target.reportValidity()) {
+      return false;
+    }
+    (async function () {
+      setLoading(true);
+      let csrf = await obtainCSRF();
+      if (!csrf) throw new Error("Cannot get CSRF");
+      //construct the request to the endpoint
+      let res = await fetchJsonWithCookie(
+        `${ENDPOINT_BASE}/auth/register`,
+        {
+          headers: {
+            ...generateCSRFHeader(csrf),
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+          body: JSON.stringify({ email }),
+        },
+        true
+      );
+      alert(res.message);
+    })().finally(() => setLoading(false));
+  }
   return (
     <div className="main-signup-container">
       <div className="left-signup-container">
@@ -19,24 +54,38 @@ export default function SignupContainer() {
         </div>
 
         <div className="email-signup">
-          <div className='signup-input-container'>
-            <h2> Enter your email</h2>
-            <form className="login-form">
-              <input
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                placeholder="Email address"
-                id="email"
-                name="email"
-              />
-              <div className="action-buttons">
-                <a className="link-btn" onClick={() => setShouldRedirect(true)}>
-                  Have an account? Log in.
-                </a>
-                <button type="submit">Sign up</button>
+          <div className="signup-input-container">
+            {loading ? (
+              <div style={{ height: "20vh" }}>
+                <JumpingRabbitLoader />
               </div>
-            </form>
+            ) : (
+              <>
+                <h2> Enter your email</h2>
+                <form className="login-form" onSubmit={performSignup}>
+                  <ValidatingInputField
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
+                    placeholder="Email address"
+                    id="email"
+                    name="email"
+                    valid={!!email}
+                    hide_label={true}
+                    error_message="The field is mandatory >_<"
+                  />
+                  <div className="action-buttons">
+                    <a
+                      className="link-btn"
+                      onClick={() => setShouldRedirect(true)}
+                    >
+                      Have an account? Log in.
+                    </a>
+                    <button type="submit">Sign up</button>
+                  </div>
+                </form>
+              </>
+            )}
           </div>
         </div>
       </div>
