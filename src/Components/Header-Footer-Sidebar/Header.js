f138f0c7a6abeb9c53e322529837a2314c2a985e
Amir <amir.zahari@besquare.com.my>
2022-11-21 23:06:08 +0800

    search function

--- src/Components/Header-Footer-Sidebar/Header.js
+++ src/Components/Header-Footer-Sidebar/Header.js
@@ -2,9 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ReduxStateConditional from "../common/ReduxStateConditional";
 import "../Header-Footer-Sidebar/Header.css";
+import { useDispatch, useSelector } from "react-redux";
+import JSONDATA from "./testdata.json";
+import Scroll from "./Scroll";
+import { setQuery } from "../../store/products/actions";
+import { LogoScaleLoader } from "../common/Loader";
 
 function Header() {
   const [open, setOpen] = React.useState(false);
+  const [searchTerm, setSearchTerm] = React.useState();
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  let dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(setQuery(searchTerm));
+    return () => {
+      dispatch(setQuery(""));
+    };
+  }, [searchTerm, dispatch]);
+  const products = useSelector((state) => state.products.products);
+
+  console.log(products);
+
   return (
     <div className="container">
       <div className="nav-container">
@@ -31,7 +52,26 @@ function Header() {
           <div className="nav-right">
             <ul className="menu">
               <li>
-                <input type="text" placeholder="Search here"></input>
+                <input
+                  type="text"
+                  placeholder="Search here"
+                  onChange={handleChange}
+                ></input>
+                <Scroll>
+                  {JSONDATA.filter((val) => {
+                    if (searchTerm == "") {
+                      return null;
+                    } else if (val.firstname.includes(searchTerm)) {
+                      return val;
+                    }
+                  }).map((val, key) => {
+                    return (
+                      <div className="users" key={key}>
+                        {val.firstname}
+                      </div>
+                    );
+                  })}
+                </Scroll>
               </li>
               <li>
                 <ReduxStateConditional
