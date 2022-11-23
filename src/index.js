6d87494609ee490048ae1f681e89d74ad6bdd5d0
Jia Jun Ngeo <jia.jun@besquare.com.my>
2022-11-23 12:00:50 +0900

    base: preload some known resources for better performance

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-
 import "./fonts/Gupter-Bold.ttf";
+import { preloadResources } from "./preload_resources.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,3 +13,5 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+preloadResources();
