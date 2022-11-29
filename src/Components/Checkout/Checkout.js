85f8f14abbebc43e11cc8c5993b2d6c91c333bea
Ikaa <nurul.syafiqah@besquare.com.my>
2022-11-29 14:33:35 +0800

    css breadcrump

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import { getCart, updateCart } from "../../store/cart/actions";
 import Header from "../Header-Footer-Sidebar/Header";
 import "./Checkout.css";
