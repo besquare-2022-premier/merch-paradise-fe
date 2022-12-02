042aab6ac09a4954e67c1cabdfc20040d05fd392
Jia Jun Ngeo <jia.jun@besquare.com.my>
2022-12-02 13:50:54 +0900

    pages: clean up some codes

--- src/Components/Main/Login.jsx
+++ src/Components/Main/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./LogSignup.css";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { performLogin } from "../../store/users/actions";
 import ReduxStateConditional from "../common/ReduxStateConditional";
@@ -9,7 +9,7 @@ import { ValidatingInputField } from "../common/ValidatingInputField";
 import { usePageTitle } from "../../utils/reactHooks";
 
 export const Login = (props) => {
-  usePageTitle("Login")
+  usePageTitle("Login");
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
   const dispatch = useDispatch();
@@ -25,13 +25,7 @@ export const Login = (props) => {
     }
     dispatch(performLogin(email, pass));
   };
-  const [shouldRedirect, setShouldRedirect] = React.useState(false);
   const state = useSelector((state) => state.user);
-
-  if (shouldRedirect) {
-    return <Navigate to="/register" />;
-  }
-
   if (state.loader_state === "failed") {
     alert(state.error.message);
     dispatch({ type: "user/wipe" });
@@ -43,15 +37,16 @@ export const Login = (props) => {
       alternative={<Navigate to="/shop#loggedin" />}
     >
       <div className="hide-mobile hide-tablet">
-        <img className="ellipse-19" src="/img/Ellipse 19.svg"></img>
-        <img className="Vector1" src="/img/Vector.svg"></img>
-        <img className="VectorLine" src="/img/Vector 1 (2).svg"></img>
+        <img className="ellipse-19" src="/img/Ellipse 19.svg" alt=""></img>
+        <img className="Vector1" src="/img/Vector.svg" alt=""></img>
+        <img className="VectorLine" src="/img/Vector 1 (2).svg" alt=""></img>
       </div>
-      <img className="logo" src="/img/LOGO.svg"></img>
+      <img className="logo" src="/img/LOGO.svg" alt="Logo"></img>
       <div className="login-container">
         <div className="login-form-wrapper">
           <img
             className="login2 hide-mobile hide-tablet"
+            alt=""
             src="/img/login2.svg"
           ></img>
           <h1 className="LoginTitle">Welcome back to MerchParadise !</h1>
@@ -80,9 +75,9 @@ export const Login = (props) => {
                 error_message="This field is mandatory"
               />
               <div className="action-buttons">
-                <a className="link-btn" onClick={() => setShouldRedirect(true)}>
+                <Link className="link-btn" to="/register">
                   Don't have an account? Sign Up.
-                </a>
+                </Link>
                 <ReduxStateConditional
                   selector={(state) => state.user.loader_state !== "loading"}
                   alternative={<JumpingRabbitLoader dataRole="submit-button" />}
